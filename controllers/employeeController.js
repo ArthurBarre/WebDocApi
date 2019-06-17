e8e668f15c61d022ca81d3fb64ee6f05dcb01a19
ArthurBArre <arthur.barre@hetic.net>
2019-06-18 01:43:01 +0200

    we have the total crud

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -3,17 +3,21 @@ var router = express.Router();
 const mongoose = require('mongoose');
 const Employee = mongoose.model('Employee');
 
-//permet de render le template addOrEdit 
+// we render the add Employee item on the "/" route
 router.get('/', (req, res) => {
-  res.render("../views/employee/addOrEdit", {
-    viewTitle: 'Insert new employee'
-  })
+  res.render("employee/addOrEdit", {
+    viewTitle: "Insert Employee"
+  });
 });
-//permet d'envoyer les données dans la db
+//Create part of the crud,
 router.post('/', (req, res) => {
-  insertRecord(req, res)
-})
-//récupere le model de db,créer un employé "avec un modèle vide“ utilses les données du form et les enregistrent avec la fonction save()
+  if (req.body._id == '')
+    insertRecord(req, res);
+  else
+    updateRecord(req, res);
+});
+
+//permet de créer un employé selon le schéma établi en récupérant la data provenenant du formulaire du component addOrEdit
 function insertRecord(req, res) {
   var employee = new Employee();
   employee.fullName = req.body.fullName;
@@ -21,49 +25,87 @@ function insertRecord(req, res) {
   employee.mobile = req.body.mobile;
   employee.city = req.body.city;
   employee.save((err, doc) => {
-    //si il n'y a pas d'erreur on me redirige vers le chemin employee/list
-    if (!err) {
-      res.redirect('/employee/list');
-    } else {
+    if (!err)
+      res.redirect('employee/list');
+    else {
       if (err.name == 'ValidationError') {
         handleValidationError(err, req.body);
-        res.render("../employee/addOrEdit", {
-          viewTitle: 'Insert new employee',
+        res.render("employee/addOrEdit", {
+          viewTitle: "Insert Employee",
           employee: req.body
-        })
-      } else {
-        console.log('oups: ' + err)
+        });
       }
-
+      else
+        console.log('Error during record insertion : ' + err);
     }
   });
 }
-//on injecte dans la route les données de la liste des employés
+//cette fonction permet en cliquant sur un l'icone modif. de récupérer l'id de l'item créer dans la base de donnée mongodb puis de l'update 
+function updateRecord(req, res) {
+  Employee.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
+    if (!err) { res.redirect('employee/list'); }
+    else {
+      if (err.name == 'ValidationError') {
+        handleValidationError(err, req.body);
+        res.render("employee/addOrEdit", {
+          viewTitle: 'Update Employee',
+          employee: req.body
+        });
+      }
+      else
+        console.log('Error during record update : ' + err);
+    }
+  });
+}
+
+//on render le component list et son contenu sur la route "/list"
 router.get('/list', (req, res) => {
   Employee.find((err, docs) => {
     if (!err) {
-      res.render('employee/list', {
+      res.render("employee/list", {
         list: docs
-      })
-    } else {
-      console.log('Errors in retrieving employee list: ' + err)
+      });
     }
-  }
-  )
-})
+    else {
+      console.log('Error in retrieving employee list :' + err);
+    }
+  });
+});
 
+//cette fonction permet de vérifier les champs fullName et email 
 function handleValidationError(err, body) {
   for (field in err.errors) {
     switch (err.errors[field].path) {
       case 'fullName':
-        body['fullNameError'] = err.errors[field].message
-        break
+        body['fullNameError'] = err.errors[field].message;
+        break;
       case 'email':
-        body['emailError'] = err.errors[field].message
-        break
+        body['emailError'] = err.errors[field].message;
+        break;
       default:
-        break
+        break;
     }
   }
 }
+//permet de render l'interface de modification de l'employé choisi
+router.get('/:id', (req, res) => {
+  Employee.findById(req.params.id, (err, doc) => {
+    if (!err) {
+      res.render("employee/addOrEdit", {
+        viewTitle: "Update Employee",
+        employee: doc
+      });
+    }
+  });
+});
+//permet de supprimer un item mongodb en connaissant son id
+router.get('/delete/:id', (req, res) => {
+  Employee.findByIdAndRemove(req.params.id, (err, doc) => {
+    if (!err) {
+      res.redirect('/employee/list');
+    }
+    else { console.log('Error in employee delete :' + err); }
+  });
+});
+
 module.exports = router;
